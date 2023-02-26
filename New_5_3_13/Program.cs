87b993cad84a1394ee332dafd89ff7f508fcc681
Andentu <severalmaz97@gmail.com>
2023-02-26 20:45:00 +0300

    Исправление

--- New_5_3_13/Program.cs
+++ New_5_3_13/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Xml;
-
-namespace New_5_3_13
+﻿namespace New_5_3_13
 {
     internal class Program
     {
@@ -8,24 +6,23 @@ namespace New_5_3_13
         {
             int[] arr = GetArrayFromConsole();
             SortArray(in arr, out int[] sorteddesc, out int[] sortedasc);
-            
+
             Console.Write("Сортировка по убыванию: ");
             foreach (int i in sorteddesc)
             {
                 Console.Write(i + " ");
             }
-            
+
             Console.Write("\nСортировка по возрастанию: ");
             foreach (int i in sortedasc)
             {
                 Console.Write(i + " ");
             }
         }
-        static void SortArray(in int [] array, out int[] sorteddesc, out int[] sortedasc)
+        static void SortArray(in int[] array, out int[] SortedDesc, out int[] SortedAsc)
         {
-            
-            sortedasc = SortArrayAsc(array);
-            sorteddesc = SortArrayDesc(array);
+            SortedDesc = SortArrayDesc(array);
+            SortedAsc = SortArrayAsc(array);
         }
         static int[] GetArrayFromConsole(int Num = 0)
         {
@@ -37,39 +34,34 @@ namespace New_5_3_13
             {
                 Console.WriteLine("Введите элемент массива номер {0}", i + 1);
                 result[i] = int.Parse(Console.ReadLine());
-
             }
             return result;
         }
-        static int[] SortArrayAsc(int[] arr) //Сортировка по возрастанию
-        {
-            
-            int num;
-            for (int i = 0; i < arr.Length; i++)
-            {
-                for (int j = i + 1; j < arr.Length; j++)
-                {
-                    if (arr[i] > arr[j])
-                    {
-                        num = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = num;
-                    }
-                }
-
-            }
-            return arr;
-
-        }
         static int[] SortArrayDesc(int[] arr) // Сортировка по убыванию
         {
-            
             int num;
             for (int i = 0; i < arr.Length; i++)
             {
                 for (int j = i + 1; j < arr.Length; j++)
                 {
                     if (arr[i] < arr[j])
+                    {
+                        num = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = num;
+                    }
+                }
+            }
+            return arr;
+        }
+        static int[] SortArrayAsc(int[] arr) //Сортировка по возрастанию
+        {
+            int num;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
                     {
                         num = arr[i];
                         arr[i] = arr[j];
